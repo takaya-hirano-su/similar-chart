01cd190d4cca51ec5ff158b8334922ec0c6c4140
takaya-hirano-su <takaya.hirano.su1120@gmail.com>
2023-04-13 22:45:20 +0900

    second-commit

--- similar_chart/settings.py
+++ similar_chart/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'similar_chart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+    "default":{
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":"similar_chart",
+        "USER":"postgres",
+        "PASSWORD":"xe7z76fr",
+        "HOST":"localhost",
+        "PORT":"5432",
     }
 }
 
