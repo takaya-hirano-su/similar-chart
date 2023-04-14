95bb2da5f891f0379d0f2f24e1d08e0490c9e532
takaya-hirano-su <takaya.hirano.su1120@gmail.com>
2023-04-14 15:39:05 +0900

    formの取引所と扱ってる通貨ペアをリンク

--- main/forms.py
+++ main/forms.py
@@ -2,14 +2,16 @@ from django import forms
 
 class Form (forms.Form):
 
-    market=forms.ChoiceField(choices=[
-        ("bitflyer","bitflyer"),("binance","binance"),
-    ],label="market")
-    pair=forms.ChoiceField(choices=[
+    market_list=[("bitflyer","bitflyer"),("binance","binance"),]
+    market=forms.ChoiceField(
+        choices=market_list,label="market",initial=market_list[0])
+    pair_list=[
         ("btcjpy","btcjpy"),("ethjpy","ethjpy"),
-        ("btcusdt","btcusdt"),("ethusdt","ethusdt"),
-    ],label="pair")
+        # ("btcusdt","btcusdt"),("ethusdt","ethusdt"),
+        ]
+    pair=forms.ChoiceField(choices=pair_list,label="pair",initial=pair_list[0])
     date=forms.DateField(
         initial="2000-01-01",
         widget=forms.NumberInput(attrs={"type":"date"}) #入力をカレンダーに設定
     )
+
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import index
+from .views import index,IndexView
 
 urlpatterns=[
-    path("",index,name="index"),
+    path("",IndexView.as_view(),name="index"),
 ]
